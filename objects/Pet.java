92c4d785a1e8eba645feb6950b9bfd4072063638
PrivateCode564 <98239215+PrivateCode564@users.noreply.github.com>
2022-02-12 16:59:43 +0200

    Pet Object

---
+++ objects/Pet.java
@@ -0,0 +1,41 @@
+package org.intellidev.ipets.objects;
+
+import org.bukkit.Location;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+//more imports
+
+
+public class Pet {
+
+    private String petName;
+    private EntityType petType;
+    private Player petOwner;
+    private Location petLocation;
+
+    public Pet(String petName, EntityType petType, Player petOwner, Location petLocation){
+        this.petName = petName;
+        this.petType = petType;
+        this.petOwner = petOwner;
+        this.petLocation = petLocation;
+    }
+
+    public EntityType getPetType(Player petOwner){
+        return PetManager.PETS.get(petOwner.getUniqueId()).petType;
+    }
+
+    public String getName(Player petOwner){
+        return PetManager.PETS.get(petOwner.getUniqueId()).petName;
+    }
+
+    public void setPetLocation(Player petOwner, Location location){
+        PetManager.PETS.get(petOwner.getUniqueId()).petLocation = location;
+
+    }
+
+    public Location getLocation(Player petOwner){
+        return PetManager.PETS.get(petOwner.getUniqueId()).petLocation;
+    }
+
+
+}
